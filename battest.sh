30303b6744affcf2b17ff4c9820952f09a35eb26
necenzurat <necenzurat@gmail.com>
2020-01-11 08:01:12 +0200

    laziness added

--- battest.sh
+++ battest.sh
@@ -1,2 +1,6 @@
-date >> ~/batlog.dat
-/usr/sbin/ioreg -l | egrep "CycleCount|Capacity" >> ~/batlog.dat
+#!/bin/bash
+location="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+date >> ${location}/batlog.dat
+/usr/sbin/ioreg -l | egrep "CycleCount|Capacity" >> ${location}/batlog.dat
